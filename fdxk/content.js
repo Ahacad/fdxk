120559a372bc584ce61532c0bdb24d40c82b2754
Ahacad <1393345075@qq.com>
2020-09-18 15:17:21 +0800

    changed js, move credits to below

--- fdxk/content.js
+++ fdxk/content.js
@@ -2,3 +2,10 @@
 //for (var i = 0, l = images.length; i < l; i++){
       //images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 //}
+
+var totalCredits = document.getElementById("selectedCredits");
+var credits = document.createElement("div");
+credits.innerHTML = totalCredits.innerHTML;
+credits.classList.add("credits");
+list = document.getElementsByClassName("gridbar");
+list[0].appendChild(credits)
