4d8a4dcf055f580b62f1a0508e5287c0d9eae71d
Ahacad <1393345075@qq.com>
2020-09-19 16:02:47 +0800

    add float div info (classroom)

--- fdxk/content.js
+++ fdxk/content.js
@@ -12,12 +12,26 @@ credits.classList.add("credits");
 list = document.getElementsByClassName("electTabsBar");
 list[0].appendChild(credits)
 
-var calendarTds = document.querySelectorAll("#courseTable table tbody tr td");
 
+var electedClasesThisSemester = document.querySelectorAll(".electTabContent tbody .defaultElected")
+function getCourseInfo(courseName){
+    for (let i = 0; i < electedClasesThisSemester.length; i++){
+        if (electedClasesThisSemester[i].children[2].innerText == courseName){
+            return electedClasesThisSemester[i].children[9].innerText
+        }
+    }
+    return ""
+}
+
+
+
+var calendarTds = document.querySelectorAll("#courseTable table tbody tr td");
 for (let i = 0; i < calendarTds.length; i++){
     let node = document.createElement("div");
     node.classList.add("div-to-display")
-    node.innerHTML = "yay";
-    calendarTds[i].append(node);
+    let innerHTML = getCourseInfo(calendarTds[i].innerText)
+    node.innerHTML = innerHTML;
+    if (calendarTds[i].innerHTML != "")
+        calendarTds[i].append(node);
 }
 
